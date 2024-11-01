3ee4d262119bce6bb2929d037a4f7313874d61cc
haikaljs <haikaljavascript@gmail.com>
2024-11-02 01:41:39 +0800

    Constants

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,4 +1,5 @@
 import Pagination from "@/Components/Pagination";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 
@@ -33,7 +34,10 @@ export default function Index({ projects }) {
                   </thead>
                   <tbody>
                     {projects.data.map((project) => (
-                      <tr key={project.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                      <tr
+                        key={project.id}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 "
+                      >
                         <td className="px-3 py-2">{project.id}</td>
                         <td className="px-3 py-2">
                           <img src={project.image_path} style={{ width: 60 }} />
@@ -42,7 +46,14 @@ export default function Index({ projects }) {
                           {project.name}
                         </td>
                         <td className="px-3 py-2 text-nowrap">
-                          {project.status}
+                          <span
+                            className={
+                              "px-2 py-1 rounded text-white " +
+                              PROJECT_STATUS_CLASS_MAP[project.status]
+                            }
+                          >
+                            {PROJECT_STATUS_TEXT_MAP[project.status]}
+                          </span>
                         </td>
                         <td className="px-3 py-2 text-nowrap">
                           {project.created_at}
